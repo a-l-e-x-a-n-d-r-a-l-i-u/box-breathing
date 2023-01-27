122083e7aade396116cc959dedea9f696d15f089
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-01-27 15:43:13 +1100

    Replace innerHTML and styles of breathBox

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 function startBoxBreath() {
   console.log("startBoxBreath function called");
+  /* Should I again declare the startButton variable - let startButton = document.getElementById("#startStop"); - inside this function, or is this a global variable that carries over from the delcaration below? */
+  startButton.style = "display: none;";
+  let fewCloudsPic = "https://bit.ly/3bwvcVS";
+  console.log("fewCloudsPic variable has been declared");
+  breathBox.style = `background-image: ${fewCloudsPic}; color: #fff; transform: scale(1.5); transition: 4s all ease-in-out;`;
+  console.log("breathBox style has been applied");
 }
 
-let startButton = document.querySelector("#startStop");
+let startButton = document.getElementById("#startStop");
+console.log("startStop variable has been declared");
+let breathBox = document.getElementById("#breathBox");
+console.log("breathBox variable has been declared");
+
 startButton.addEventListener(
   "click",
   startBoxBreath
