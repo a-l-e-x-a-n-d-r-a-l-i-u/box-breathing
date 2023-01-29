4e58fd403e3972dd91f6374e8f9bf1ae31c68f9e
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-01-29 18:10:49 +1100

    Use setTimeout method as placeholder
    - Animate single box breath using setTimeout for text and image size transitions.
    
    Until I learn a better JS method (if condition? event listener?)

--- js/main.js
+++ js/main.js
@@ -1,21 +1,59 @@
-setTimeout(function holdBreath1() {
+setTimeout(function showStartButttonAgain() {
+  /* Show start button */
+  let startButton = document.getElementById("#startStop");
+  startButton.style = "display: inline-block;";
+  startButton.innerHTML = "again";
+}, 17000);
+
+setTimeout(function startOver() {
+  /* Remove text */
+  let instruction = document.getElementById("#breathCount");
+  instruction.style = "display: none;";
+}, 16000);
+
+setTimeout(function holdBreath2() {
   /* Change instruction to hold breath */
-  let counting = document.getElementById("#breathCount");
-  counting.innerHTML = "hold ...";
+  let instruction = document.getElementById("#breathCount");
+  instruction.innerHTML = "hold ...";
+
+  /* Gradually return to the original image */
+  let box = document.getElementById("#breathe");
+  box.style = `transition: 4s all ease-in-out;`;
+}, 12000);
+
+setTimeout(function breatheOut() {
+  /* Change instruction to breathe out */
+  let instruction = document.getElementById("#breathCount");
+  instruction.innerHTML = "breathe out";
+
+  /* Keep the fewCloudsPic showing */
+  let fewCloudsPic = "url(https://bit.ly/3bwvcVS)";
+
+  /* Slowly decrease the breathbox size*/
+  let box = document.getElementById("#breathe");
+  box.style = `background-image: ${fewCloudsPic}; transform: scale(1); transition: 4s all ease-in-out;`;
+  holdBreath2();
 }, 8000);
 
-function breatheIn1() {
-  let breatheInCount1 = document.getElementById("#breathe");
+setTimeout(function holdBreath1() {
+  /* Change instruction to hold breath */
+  let instruction = document.getElementById("#breathCount");
+  instruction.innerHTML = "hold ...";
+  breatheOut();
+}, 4000);
+
+function breatheIn() {
+  let box = document.getElementById("#breathe");
 
   /* Keep the fewCloudsPic showing */
   let fewCloudsPic = "url(https://bit.ly/3bwvcVS)";
 
   /* Slowly increase the breathbox size*/
-  breatheInCount1.style = `background-image: ${fewCloudsPic}; transform: scale(2); transition: 6s all ease-in-out;`;
+  box.style = `background-image: ${fewCloudsPic}; transform: scale(2); transition: 4s all ease-in-out;`;
 
   /* Show instruction to breathe in */
-  let counting = document.getElementById("#breathCount");
-  counting.innerHTML = "breathe in ...";
+  let instruction = document.getElementById("#breathCount");
+  instruction.innerHTML = "breathe in";
 
   /* Hold your breath */
   holdBreath1();
@@ -29,8 +67,8 @@ function hideButton() {
   startButton.style = "display: none;";
 
   /* Start the breathbox 'animation' and counting */
-  breatheIn1();
-  console.log("breatheIn1 function was called from within startBoxBreath");
+  breatheIn();
+  console.log("breatheIn function was called from within startBoxBreath");
 }
 
 function changeImage() {
