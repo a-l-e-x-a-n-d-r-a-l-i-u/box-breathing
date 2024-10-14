f65f43e87a93ce06bc03171dfcdd2be7da2daab5
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-14 21:22:51 +1100

    Compartmentalised the functions

--- js/main.js
+++ js/main.js
@@ -6,6 +6,18 @@ function updateElement(id, styles = {}, innerHTML = null) {
   }
 }
 
+function changeImage() {
+  updateElement("#breathe", {
+    backgroundImage: "url(https://jooinn.com/images/beautiful-clouds.jpg)",
+  });
+  hideButton();
+}
+
+function hideButton() {
+  updateElement("#startStop", { display: "none" });
+  breatheIn();
+}
+
 function breatheIn() {
   updateElement("breathe", {
     backgroundImage: "url(https://jooinn.com/images/beautiful-clouds.jpg)", //fewCloudsPic
@@ -13,78 +25,38 @@ function breatheIn() {
     transition: "4s all ease-in-out",
   });
   updateElement("breathCount", {}, "breathe in");
-  setTimeout(holdBreath1, 4000);
+  setTimeout(holdBreathIn, 4000);
 }
 
-setTimeout(function showStartButttonAgain() {
-  /* Show start button */
-  let startButton = document.getElementById("#startStop");
-  startButton.style = "display: inline-block;";
-  startButton.innerHTML = "again";
-}, 17000);
-
-setTimeout(function startOver() {
-  /* Remove text */
-  let instruction = document.getElementById("#breathCount");
-  instruction.style = "display: none;";
-}, 16000);
-
-setTimeout(function holdBreath2() {
-  /* Change instruction to hold breath */
-  let instruction = document.getElementById("#breathCount");
-  instruction.innerHTML = "hold ...";
-
-  /* Gradually return to the original image */
-  let box = document.getElementById("#breathe");
-  box.style = `transition: 4s all ease-in-out;`;
-}, 12000);
-
-setTimeout(function breatheOut() {
-  /* Change instruction to breathe out */
-  let instruction = document.getElementById("#breathCount");
-  instruction.innerHTML = "breathe out";
-
-  /* Keep the fewCloudsPic showing */
-  let fewCloudsPic = "url(https://jooinn.com/images/beautiful-clouds.jpg)";
-
-  /* Slowly decrease the breathbox size*/
-  let box = document.getElementById("#breathe");
-  box.style = `background-image: ${fewCloudsPic}; transform: scale(1); transition: 4s all ease-in-out;`;
-  holdBreath2();
-}, 8000);
-
-function holdBreath1() {
-  /* Change instruction to hold breath */
-  let instruction = document.getElementById("#breathCount");
-  instruction.innerHTML = "hold ...";
-  breatheOut();
+function holdBreathIn() {
+  updateElement("#breathCount", {}, "hold ...");
+  setTimeout(breatheOut, 8000);
 }
 
-function hideButton() {
-  /* Should I again declare the startButton variable inside this function? Or is this a global variable that carries over from the declaration below (outside of the function)? */
-
-  let startButton = document.getElementById("#startStop");
-  console.log("startStop variable has been declared within the function");
-  startButton.style = "display: none;";
-
-  /* Start the breathbox 'animation' and counting */
-  breatheIn();
-  console.log("breatheIn function was called from within startBoxBreath");
+function breatheOut() {
+  updateElement("#breathCount", {}, "breathe out");
+  updateElement("breathe", {
+    backgroundImage: "url(https://jooinn.com/images/beautiful-clouds.jpg)",
+    transform: "scale(1)",
+    transition: "4s all ease-in-out",
+  });
+  setTimeout(holdBreathOut, 12000);
 }
 
-function changeImage() {
-  /* Change the background image first to a local file to try to reduce lag time */
-  let fewCloudsPic = "url(https://jooinn.com/images/beautiful-clouds.jpg)";
-  let currentImage = document.getElementById("#breathe");
-  currentImage.style = `background-image: ${fewCloudsPic};`;
-
-  /* Hide the start button after it is clicked */
-  hideButton();
+function holdBreathOut() {
+  updateElement("#breathCount", {}, "hold ...");
+  updateElement("breathe" {
+    transition: "4s all ease-in-out"
+  })
 }
 
-/* Does declaring this as a variable outside of the functions allow the functions inside to read this variable? Or is this a wasted line of code here? */
-let startButton = document.getElementById("#startStop");
-console.log("startStop variable has been declared");
+function startOver() {
+  updateElement("#breathCount", { display: "none" });
+  setTimeout(showStartButtonAgain, 1000);
+}
 
-/* When user the clicks the start button, begin the box-breath 'animation'*/
-startButton.addEventListener("click", changeImage);
+function showStartButtonAgain() {
+  updateElement("#startStop", { display: "inline-block" }, "again");
+}
+
+document.getElementById("#startStop").addEventListener("click", changeImage);
